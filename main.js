cd08ff980e82af1a92c42d2796c4ce5d3595cb2b
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-25 17:38:45 +0100

    feat: add bonus tasks

--- main.js
+++ main.js
@@ -55,11 +55,17 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+// BONUS
+
+// 1. Formattare le date in formato italiano (gg/mm/aaaa)
+
+// 2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
+
+// 3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
 
 // Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
 
 // Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
-
 
 // Stampo in pagina tutti i post presenti nell'array di oggetti
 // Creo l'elemento del DOM che andrò a popolare
@@ -83,45 +89,45 @@ posts.forEach((singlePost) => {
             })
         });
     
-    // FUNCTIONS
-    
-    // funzione per generare il singolo template
-    // createNewPost oggetto che passa le chiavi alla funzione
-    // return: singolo post template
-    function singleTemplate (createNewPost){
-        // Destrutturo gli oggetti per poter utilizzare le variabili nel DOM
-        let {id , content, media, author, likes, created} = createNewPost;
-        let newPost =`
-        <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${author.image}" alt="Phil Mangione">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${author.name}</div>
-                        <div class="post-meta__time">${created}</div>
-                    </div>                    
+// FUNCTIONS
+
+// funzione per generare il singolo template
+// createNewPost oggetto che passa le chiavi alla funzione
+// return: singolo post template
+function singleTemplate (createNewPost){
+    // Destrutturo gli oggetti per poter utilizzare le variabili nel DOM
+    let {id , content, media, author, likes, created} = createNewPost;
+    let newPost =`
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${author.image}" alt="Phil Mangione">                    
                 </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${author.name}</div>
+                    <div class="post-meta__time">${created}</div>
+                </div>                    
             </div>
-            <div class="post__text">${content}</div>
-            <div class="post__image">
-                <img src="${media}" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
-                    </div>
-                </div> 
-            </div>            
         </div>
-        `;
-        return newPost;
-    }
+        <div class="post__text">${content}</div>
+        <div class="post__image">
+            <img src="${media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `;
+    return newPost;
+}
